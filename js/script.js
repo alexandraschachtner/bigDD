5262d174101fcd0efc22136512bbf0ee8ca3bdab
Alexandra <alexandra.schachtner@gmail.com>
2018-11-07 12:44:56 -0600

    form inputs now accurate for a real site.  <p> and <h 2345> color changed, submit button more visable

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-// //padding for navbar//
-// var shiftWindow = function() { scrollBy(0, -70) };
-// if (location.hash) shiftWindow();
-// window.addEventListener("hashchange", shiftWindow);
 
 //navbar collapse automatically when a link is clicked//
 $('.navbar-collapse a:not(.dropdown-toggle)').click(function(){
     $(this).parents('.navbar-collapse').collapse('hide');
 });
 
+//function to offer differenting alert messages dependent on the checkbox status in conjunction with the submit button//
 function couponCode() {
   var checkBox = document.getElementById ("exampleCheck1")
 
-if (exampleCheck1.checked == true){
+if (checkNews.checked == true){
   alert("Thank you for subscribing!  Here's a coupon code just for you:  FREEDONUT");
 } else {
   alert("Thanks!  We will get back to you as soon as possible");
