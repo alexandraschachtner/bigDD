8bde98afb10835a5267ff7a4c6f6f2fe420b76bb
Alexandra <alexandra.schachtner@gmail.com>
2018-11-12 13:14:30 -0600

    alert from contact now takes user to a new window if they checked the box

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@
 $('.navbar-collapse a:not(.dropdown-toggle)').click(function(){
     $(this).parents('.navbar-collapse').collapse('hide');
 });
+
 //function to offer differenting alert messages dependent on the checkbox status in conjunction with the submit button//
 function couponCode() {
-  var checkBox = document.getElementById ("exampleCheck1")
+  var checkBox = document.getElementById ("checkNews")
 
 if (checkNews.checked == true){
-  alert("Thank you for subscribing!  Here's a coupon code just for you:  FREEDONUT");
+  alert("Thank you for subscribing!")
+  window.open("couponcode.html");
 } else {
   alert("Thanks!  We will get back to you as soon as possible");
 }
